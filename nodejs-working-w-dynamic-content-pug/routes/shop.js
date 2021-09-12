8f7bc9938669a71dcfe29be85c380d9a5c840182
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-09-12 16:41:54 +0300

    setting up pug files

--- nodejs-working-w-dynamic-content-pug/routes/shop.js
+++ nodejs-working-w-dynamic-content-pug/routes/shop.js
@@ -8,8 +8,8 @@ const router = express.Router();
 const adminData = require('./admin');
 
 router.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.render('shop');
+  const products = adminData.products;
+  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
 });
 
 module.exports = router;
