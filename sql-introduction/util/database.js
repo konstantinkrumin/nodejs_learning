c9df7950b0bc079980471b292882c55789aeba76
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-13 20:48:58 +0300

    added database password config

--- sql-introduction/util/database.js
+++ sql-introduction/util/database.js
@@ -1,10 +1,12 @@
+const DATABASE_PASSWORD = require('../config/database_password');
+
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'node-complete',
-  password: 'nodecomplete',
+  password: DATABASE_PASSWORD,
 });
 
 module.exports = pool.promise();
