b9ab729f641773a96fe90496ebdc817d14e5421f
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-14 18:01:01 +0300

    finished setting up the connection to mongodb

--- working-w-nosql-and-using-mongodb/util/database.js
+++ working-w-nosql-and-using-mongodb/util/database.js
@@ -3,13 +3,27 @@ const DATABASE_PASSWORD = require('../config/database_password');
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
-  MongoClient.connect(`mongodb+srv://costa:${DATABASE_PASSWORD}@cluster0.zk7do.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+  MongoClient.connect(`mongodb+srv://costa:${DATABASE_PASSWORD}@cluster0.zk7do.mongodb.net/shop?retryWrites=true&w=majority`)
     .then((client) => {
       console.log('Connected to MongoDB');
-      callback(client);
+      _db = client.db();
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
