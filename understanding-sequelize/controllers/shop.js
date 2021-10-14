79f8575a34195a6bab9ea5f5da32ff67728b251e
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-14 16:29:20 +0300

    added orders flow in sequelize

--- understanding-sequelize/controllers/shop.js
+++ understanding-sequelize/controllers/shop.js
@@ -124,9 +124,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
+  let fetchedCart;
   req.user
     .getCart()
     .then((cart) => {
+      fetchedCart = cart;
       return cart.getProducts();
     })
     .then((products) => {
@@ -143,6 +145,9 @@ exports.postOrder = (req, res, next) => {
           );
         })
         .then((result) => {
+          fetchedCart.setProducts(null);
+        })
+        .then((result) => {
           res.redirect('/orders');
         })
         .catch((err) => console.log(err));
@@ -151,15 +156,14 @@ exports.postOrder = (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders',
-  });
-};
-
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout',
-  });
+  req.user
+    .getOrders({ include: ['products'] })
+    .then((orders) => {
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
 };
