02029a7d48e79643bd13714f63ab51ccf762f0b6
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-14 12:53:42 +0300

    set up adding and retrieving items from the cart

--- understanding-sequelize/app.js
+++ understanding-sequelize/app.js
@@ -43,8 +43,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
     return User.findByPk(1);
     // console.log(result);
@@ -60,6 +60,9 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => {
