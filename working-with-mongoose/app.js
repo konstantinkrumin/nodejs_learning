11ec49ad8af9d70679c3f575491e0164ca15244f
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-18 14:08:31 +0300

    set up the User model

--- working-with-mongoose/app.js
+++ working-with-mongoose/app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const DATABASE_PASSWORD = require('./config/database_password');
 
@@ -20,14 +20,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('5baa2528563f16379fc8a610')
-//     .then((user) => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('616d546cacadbd4b88960d69')
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,6 +37,18 @@ app.use(errorController.get404);
 mongoose
   .connect(`mongodb+srv://costa:${DATABASE_PASSWORD}@cluster0.zk7do.mongodb.net/shop?retryWrites=true&w=majority`)
   .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: 'Costa',
+          email: 'costa@email.com',
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
   })
   .catch((err) => {
