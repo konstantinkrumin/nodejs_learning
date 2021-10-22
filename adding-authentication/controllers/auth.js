0bf8e28a3216a51b6c013843da23a0355b84726d
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-22 23:45:30 +0300

    added additional error messages

--- adding-authentication/controllers/auth.js
+++ adding-authentication/controllers/auth.js
@@ -3,17 +3,32 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: req.flash('error'),
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
+    errorMessage: message,
   });
 };
 
@@ -41,7 +56,7 @@ exports.postLogin = (req, res, next) => {
           res.redirect('/login');
         })
         .catch((err) => {
-          console.log(err);
+          req.flash('error', 'Invalid email or password.');
           res.redirect('/login');
         });
     })
@@ -56,6 +71,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash('error', 'Email already exists, please pick a different one');
         return res.redirect('/signup');
       }
       return bcrypt
