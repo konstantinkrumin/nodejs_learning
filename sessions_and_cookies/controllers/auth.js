34b751aa34894962180d60f87b17c160ee0f374a
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-22 14:07:54 +0300

    set up deleting session on logout

--- sessions_and_cookies/controllers/auth.js
+++ sessions_and_cookies/controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1] === 'true';
   const isLoggedIn = req.session.isLoggedIn;
@@ -11,6 +13,18 @@ exports.getLogin = (req, res, next) => {
 
 exports.postLogin = (req, res, next) => {
   // res.setHeader('Set-Cookie', 'loggedIn=true');
-  req.session.isLoggedIn = true;
-  res.redirect('/');
+  User.findById('616d546cacadbd4b88960d69')
+    .then((user) => {
+      req.session.user = user;
+      req.session.isLoggedIn = true;
+      res.redirect('/');
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
