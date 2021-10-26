a07c17c45342cafb9f479d40d471b88bd3905255
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-26 21:50:01 +0300

    added validation on login and sanitization on login and signup

--- 18_understanding-validation/routes/auth.js
+++ 18_understanding-validation/routes/auth.js
@@ -13,8 +13,8 @@ router.get('/signup', authController.getSignup);
 router.post(
   '/login',
   [
-    check('email').isEmail().withMessage('Please enter a valid email'),
-    body('password', 'Please enter a password with only numbers and text and at least 5 characters').isLength({ min: 5 }).isAlphanumeric(),
+    check('email').isEmail().withMessage('Please enter a valid email').normalizeEmail(),
+    body('password', 'Please enter a password with only numbers and text and at least 5 characters').isLength({ min: 5 }).isAlphanumeric().trim(),
   ],
   authController.postLogin
 );
@@ -31,14 +31,17 @@ router.post(
             return Promise.reject('E-Mail exists already, please pick a different one.');
           }
         });
+      })
+      .normalizeEmail(),
+    body('password', 'Please enter a password with only numbers and text and at least 5 characters').isLength({ min: 5 }).isAlphanumeric().trim(),
+    body('confirmPassword')
+      .trim()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match!');
+        }
+        return true;
       }),
-    body('password', 'Please enter a password with only numbers and text and at least 5 characters').isLength({ min: 5 }).isAlphanumeric(),
-    body('confirmPassword').custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error('Passwords have to match!');
-      }
-      return true;
-    }),
   ],
 
   authController.postSignup
