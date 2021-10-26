f09d479fbc4bdefed4d9c29a1b5ec7b0b36c30e2
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-26 19:29:09 +0300

    modified login validation

--- 18_understanding-validation/controllers/auth.js
+++ 18_understanding-validation/controllers/auth.js
@@ -48,6 +48,17 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
@@ -79,6 +90,7 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors.array());
