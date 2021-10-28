a8f6c74437dc8f5baa13b1c7273cebd736efefee
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-28 18:29:24 +0300

    added stripe to handle payments

--- 23_adding-payments/controllers/shop.js
+++ 23_adding-payments/controllers/shop.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
+const STRIPE_API_KEY = require('../config/stripe_api_key');
+const stripe = require('stripe')(STRIPE_API_KEY);
+
 const PDFDocument = require('pdfkit');
 
 const Product = require('../models/product');
@@ -139,7 +142,51 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then((user) => {
+      products = user.cart.items;
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price;
+      });
+
+      return stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: products.map((p) => {
+          return {
+            name: p.productId.title,
+            description: p.productId.description,
+            amount: p.productId.price * 100,
+            currency: 'usd',
+            quantity: p.quantity,
+          };
+        }),
+        success_url: req.protocol + '://' + req.get('host') + '/checkout/success',
+        cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
+      });
+    })
+    .then((session) => {
+      res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products: products,
+        totalSum: total,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
     .execPopulate()
