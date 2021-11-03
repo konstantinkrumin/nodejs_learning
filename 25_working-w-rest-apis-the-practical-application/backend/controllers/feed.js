e20a2dbb3ca1eae0698bfb7ff390f32796c33aad
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-03 22:57:46 +0300

    added clean up of the connections between users and posts"

--- 25_working-w-rest-apis-the-practical-application/backend/controllers/feed.js
+++ 25_working-w-rest-apis-the-practical-application/backend/controllers/feed.js
@@ -186,8 +186,14 @@ exports.deletePost = (req, res, next) => {
       clearImage(post.imageUrl);
       return Post.findByIdAndRemove(postId);
     })
-    .then((result) => {
-      console.log(result);
+    .then(() => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then(() => {
       res.status(200).json({ message: 'Deleted post.' });
     })
     .catch((err) => {
