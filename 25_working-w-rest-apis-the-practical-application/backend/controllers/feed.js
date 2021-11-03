9796bf1a448417fb0c38d1149fcc6693c0657cf8
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-03 22:34:44 +0300

    added connection between posts and users

--- 25_working-w-rest-apis-the-practical-application/backend/controllers/feed.js
+++ 25_working-w-rest-apis-the-practical-application/backend/controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -49,20 +50,33 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   const imageUrl = req.file.path.replace('\\', '/');
+  let creator;
+
   const post = new Post({
     title: title,
     imageUrl: imageUrl,
     content: content,
-    creator: {
-      name: 'Costa',
-    },
+    creator: req.userId,
   });
+
   post
     .save()
+    .then(() => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
     .then((result) => {
       res.status(201).json({
         message: 'Post created successfully!',
-        post: result,
+        post: post,
+        creator: {
+          _id: creator._id,
+          name: creator.name,
+        },
       });
     })
     .catch((err) => {
