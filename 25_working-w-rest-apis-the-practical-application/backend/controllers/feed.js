60df7a069d57e13921225706e2bf8140a31e856c
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-03 16:56:51 +0300

    setup fetching a single post

--- 25_working-w-rest-apis-the-practical-application/backend/controllers/feed.js
+++ 25_working-w-rest-apis-the-practical-application/backend/controllers/feed.js
@@ -3,26 +3,25 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/imac.jpg',
-        creator: {
-          name: 'Costa',
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({
+        message: 'Fetched posts successfully',
+        posts: posts,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const error = new Error('Validation failed, entered data is incorret.');
+    const error = new Error('Validation failed, entered data is incorrect.');
     error.statusCode = 422;
     throw error;
   }
@@ -44,6 +43,26 @@ exports.createPost = (req, res, next) => {
         message: 'Post created successfully!',
         post: result,
       });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error('Could not find post');
+        error.statusCode = 404;
+        throw error;
+      }
+
+      res.status(200).json({ message: 'Post fetched', post: post });
     })
     .catch((err) => {
       if (!err.statusCode) {
--- 25_working-w-rest-apis-the-practical-application/backend/routes/feed.js
+++ 25_working-w-rest-apis-the-practical-application/backend/routes/feed.js
@@ -11,4 +11,6 @@ router.get('/posts', feedController.getPosts);
 // POST /feed/post
 router.post('/post', [body('title').trim().isLength({ min: 5 }), body('content').trim().isLength({ min: 5 })], feedController.createPost);
 
+router.get('/post/:postId', feedController.getPost);
+
 module.exports = router;
