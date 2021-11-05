e709f44c43dbd55548911a66cceb90941087c86d
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-05 15:56:59 +0300

    setup socket real time on update

--- 27_understanding-websockets-and-socket-io/backend/controllers/feed.js
+++ 27_understanding-websockets-and-socket-io/backend/controllers/feed.js
@@ -124,7 +124,7 @@ exports.updatePost = async (req, res, next) => {
       throw error;
     }
 
-    const post = await Post.findById(postId);
+    const post = await Post.findById(postId).populate('creator');
 
     if (!post) {
       const error = new Error('Could not find post');
@@ -132,7 +132,7 @@ exports.updatePost = async (req, res, next) => {
       throw error;
     }
 
-    if (post.creator.toString() !== req.userId) {
+    if (post.creator._id.toString() !== req.userId) {
       const error = new Error('Not authorized');
       error.statusCode = 403;
       throw error;
@@ -147,6 +147,11 @@ exports.updatePost = async (req, res, next) => {
     post.content = content;
 
     const result = await post.save();
+
+    io.getIO().emit('posts', {
+      action: 'update',
+      post: result,
+    });
 
     res.status(200).json({
       message: 'Post updated!',
