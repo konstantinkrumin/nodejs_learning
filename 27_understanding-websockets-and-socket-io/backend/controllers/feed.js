a2748412165bd14c83e774bb8ba030d46535c8ba
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-05 16:02:52 +0300

    added updating, deletion and sorting on socket.io action

--- 27_understanding-websockets-and-socket-io/backend/controllers/feed.js
+++ 27_understanding-websockets-and-socket-io/backend/controllers/feed.js
@@ -15,6 +15,7 @@ exports.getPosts = async (req, res, next) => {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
       .populate('creator')
+      .sort({ createdAt: -1 })
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
 
@@ -189,6 +190,11 @@ exports.deletePost = async (req, res, next) => {
     const user = await User.findById(req.userId);
     user.posts.pull(postId);
     await user.save();
+
+    io.getIO().emit('posts', {
+      action: 'delete',
+      post: postId,
+    });
 
     res.status(200).json({ message: 'Deleted post.' });
   } catch (err) {
