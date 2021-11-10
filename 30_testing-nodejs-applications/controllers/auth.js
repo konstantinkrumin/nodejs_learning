953d92e24759d1fdd6f97fd05fefcea87b5c6091
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-10 16:24:43 +0300

    set up async testing of the auth controller login

--- 30_testing-nodejs-applications/controllers/auth.js
+++ 30_testing-nodejs-applications/controllers/auth.js
@@ -21,7 +21,7 @@ exports.signup = async (req, res, next) => {
     const user = new User({
       email: email,
       password: hashedPw,
-      name: name
+      name: name,
     });
     const result = await user.save();
     res.status(201).json({ message: 'User created!', userId: result._id });
@@ -54,17 +54,19 @@ exports.login = async (req, res, next) => {
     const token = jwt.sign(
       {
         email: loadedUser.email,
-        userId: loadedUser._id.toString()
+        userId: loadedUser._id.toString(),
       },
       'somesupersecretsecret',
       { expiresIn: '1h' }
     );
     res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+    return;
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
+    return err;
   }
 };
 
