1d226d795064b5d0061b4572697391343aca5ae0
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-11-14 16:30:16 +0300

    built a simple api with nodejs

--- 34_intro-to-deno/app.js
+++ 34_intro-to-deno/app.js
@@ -1,8 +1,16 @@
 // EXAMPLE WITH NODEJS
-const fs = require('fs').promises;
+// const fs = require('fs').promises;
 
-const text = 'This is the same example but using Node.js instead';
+// const text = 'This is the same example but using Node.js instead';
 
-fs.writeFile('node-message.txt', text).then(() => {
-  console.log('Wrote file!');
+// fs.writeFile('node-message.txt', text).then(() => {
+//   console.log('Wrote file!');
+// });
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello world');
 });
+
+server.listen(3000);
--- 34_intro-to-deno/app.ts
+++ 34_intro-to-deno/app.ts
@@ -1,9 +1,27 @@
 // EXAMPLE WITH DENO
-const text = 'This is a test and should be stored in a file';
+// const text = 'This is a test and should be stored in a file';
 
-const encoder = new TextEncoder();
-const data = encoder.encode(text);
+// const encoder = new TextEncoder();
+// const data = encoder.encode(text);
 
-Deno.writeFile('message.txt', data).then(() => {
-  console.log('Wrote to file');
+// Deno.writeFile('message.txt', data).then(() => {
+//   console.log('Wrote to file');
+// });
+
+// import { serve } from 'https://deno.land/std@0.114.0/http/server_legacy.ts';
+
+// const server = serve({ port: 3000 });
+
+// for await (const req of server) {
+//   req.respond({ body: 'Hello World\n' });
+// }
+
+import { Application } from 'https://deno.land/x/oak/mod.ts';
+
+const app = new Application();
+
+app.use((ctx: any) => {
+  ctx.response.body = 'Hello world!';
 });
+
+await app.listen({ port: 8000 });
